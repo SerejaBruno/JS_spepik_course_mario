412dc0829cfc7ec3fe2c89b165e41ef69ca6dc7f
SerejaBruno <gordooniy@gmail.com>
2021-07-02 19:26:42 +0300

    stairs reworked

--- mario.js
+++ mario.js
@@ -1,9 +1,23 @@
 
 printPyramid(5);
+// " ".repeat(height - i - 1) + "#".repeat(i + 2)) + "\n"
+
+
+
 
 
 function printPyramid(height) {
+    let stairs = "";
     for (let i = 0; i < height; i++){
-            console.log(" ".repeat(height - i - 1) + "#".repeat(i + 2));
-        }
+        stairs = stairs.concat(('&nbsp&nbsp'.repeat(height - i - 1) + "#".repeat(i + 2)) + `<br>`)
+    }
+    return stairs;
 }
+
+console.log(printPyramid(8))
+let bricks = document.getElementById('pyramid');
+bricks.insertAdjacentHTML("afterbegin", printPyramid(15))
+
+let construction = document.getElementById("construction");
+construction.remove();
+
